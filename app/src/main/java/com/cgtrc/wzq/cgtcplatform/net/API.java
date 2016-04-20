b33aaedc599b7726d75852007cadfede7e04df60
bym <wzqhbkjdx@126.com>
2016-04-20 10:24:51 +0800

    remove url

--- app/src/main/java/com/cgtrc/wzq/cgtcplatform/net/API.java
+++ app/src/main/java/com/cgtrc/wzq/cgtcplatform/net/API.java
@@ -4,7 +4,7 @@ package com.cgtrc.wzq.cgtcplatform.net;
  * Created by bym on 16/3/24.
  */
 public class API {
-    public static final String BASE_URL = "http://123.57.37.128";
+    public static final String BASE_URL = "";
     public static final String TEST_URL = "http://172.28.131.11:8080";
     // request type, latest stands for getting newly information
     public static final int TYPE_LATEST = 0;
